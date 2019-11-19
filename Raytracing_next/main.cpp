8edfa77b856d52c9f1041e197e7a5f7cc9578533
十木 <jukisan0525@gmail.com>
2019-11-19 23:22:07 +0800

    解决bvh_node排序内存访问错误bug

--- Raytracing_next/main.cpp
+++ Raytracing_next/main.cpp
@@ -12,10 +12,11 @@
 #include "translate.h"
 #include "rotate.h"
 #include "fog.h"
+#include "bvh_node.h"
 
-const int width = 1024;
-const int height = 512;
-const int sampleNum = 10;
+const int width = 800;
+const int height = 800;
+const int sampleNum = 100;
 
 vec3 paint(const ray& r, hitable* world, int depth);
 hitable* scene();
@@ -24,6 +25,7 @@ hitable* image_sphere();
 hitable* light_sphere();
 hitable* room();
 hitable* fog_room();
+hitable* final();
 
 int main()
 { 
@@ -37,12 +39,14 @@ int main()
 	file << "P3\n" << width << " " << height << std::endl << "255" << std::endl;
 	/*vec3 lookfrom(278.0f, 278.0f, -800.0f);
 	vec3 lookat(278.0f, 278.0f, 0.0f);*/
-	vec3 lookfrom(0, 0, 6);
-	vec3 lookat(0,0,0);
+	/*vec3 lookfrom(0, 0, 6);
+	vec3 lookat(0,0,0);*/
+	vec3 lookfrom(478, 278, -600);
+	vec3 lookat(278, 278, 0);
 	float distance_to_focus = 10.0f;
 	float aperture = 0.0f;
 	camera c(lookfrom, lookat, vec3(0.0f, 1.0f, 0.0f), 40.0f, float(width) / float(height), aperture, distance_to_focus, 0.0f, 1.0f);
-	hitable* world = image_sphere();
+	hitable* world = final();
 	for (int i = height - 1; i >= 0; i--)
 	{
 		for (int j = 0; j < width; j++)
@@ -80,16 +84,16 @@ vec3 paint(const ray& r, hitable* world, int depth)
 		}
 		else 
 		{
-			//return emit;
-			return vec3(0.0f, 0.0f, 0.0f);
+			return emit;
+			//return vec3(0.0f, 0.0f, 0.0f);
 		}
 	}
 	else 
 	{
-		vec3 unit_direction = normalized_vector(r.direction());
+		/*vec3 unit_direction = normalized_vector(r.direction());
 		float t = 0.5 * (unit_direction.y() + 1.0);
-		return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
-		//return vec3(0.0f, 0.0f, 0.0f);
+		return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);*/
+		return vec3(0.0f, 0.0f, 0.0f);
 	}
 }
 
@@ -161,6 +165,55 @@ hitable* fog_room()
 	list[i++] = new constant_medium(b2, 0.01, new const_texture(vec3(0.0, 0.0, 0.0)));
 
 	return new hitable_list(list, i);
+}
+
+hitable* final() 
+{
+	int nb = 20;
+	hitable** list = new hitable * [30];
+	hitable** boxlist = new hitable * [10000];
+	hitable** boxlist2 = new hitable * [10000];
+	material* white = new lambertian(new const_texture(vec3(0.73, 0.73, 0.73)));
+	material* ground = new lambertian(new const_texture(vec3(0.48, 0.83, 0.53)));
+	int b = 0;
+	for (int i = 0; i < nb; i++) 
+	{
+		for (int j = 0; j < nb; j++) 
+		{
+			float w = 100;
+			float x0 = -1000 + i * w;
+			float z0 = -1000 + j * w;
+			float y0 = 0;
+			float x1 = x0 + w;
+			float y1 = 100 * (random_float() + 0.01);
+			float z1 = z0 + w;
+			boxlist[b++] = new box(vec3(x0, y0, z0), vec3(x1, y1, z1), ground);
+		}
+	}
+	int l = 0;
+	list[l++] = new bvh_node(boxlist, b, 0, 1);
+	material* light = new diffuse_light(new const_texture(vec3(7, 7, 7)));
+	list[l++] = new xz_rectangle(123, 423, 147, 412, 554, light);
+	vec3 center(400, 400, 200);
+	list[l++] = new move_sphere(50, center, center + vec3(30, 0, 0), new lambertian(new const_texture(vec3(0.7, 0.3, 0.1))), 0, 1);
+	list[l++] = new sphere(50, vec3(260, 150, 45), new dielectric(1.5));
+	list[l++] = new sphere(50, vec3(0, 150, 145), new metal(vec3(0.8, 0.8, 0.9), 10.0));
+	hitable* boundary = new sphere(70, vec3(360, 150, 145), new dielectric(1.5));
+	list[l++] = boundary;
+	list[l++] = new constant_medium(boundary, 0.2, new const_texture(vec3(0.2, 0.4, 0.9)));
+	boundary = new sphere(5000, vec3(0, 0, 0), new dielectric(1.5));
+	list[l++] = new constant_medium(boundary, 0.0001, new const_texture(vec3(1.0, 1.0, 1.0)));
+	material* emat = new lambertian(new image_texture("earthmap.jpg"));
+	list[l++] = new sphere(100, vec3(400, 200, 400), emat);
+	texture* pertext = new noise_texture(0.1);
+	list[l++] = new sphere(80, vec3(220, 280, 300), new lambertian(pertext));
+	int ns = 1000;
+	for (int j = 0; j < ns; j++) 
+	{
+		boxlist2[j] = new sphere(10, vec3(165 * random_float(), 165 * random_float(), 165 * random_float()), white);
+	}
+	list[l++] = new translate(new rotate_y(new bvh_node(boxlist2, ns, 0.0, 1.0), 15), vec3(-100, 270, 395));
+	return new hitable_list(list, l);
 }
 
 hitable* scene()
